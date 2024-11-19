3d18116c00fdf3bbd153b4b1c34d34110d6425e0
amazingly145 <A01753419@tec.mx>
2024-11-18 18:29:15 -0600

    Update EasyBoard.cpp

--- EasyBoard.cpp
+++ EasyBoard.cpp
@@ -1,6 +1,7 @@
 #include "EasyBoard.h"
 #include "Board.h"
 #include "Board.cpp"
+#include "Card.cpp"
 #include <string>
 using namespace std;
 #include <iostream>
@@ -12,9 +13,8 @@ EasyBoard::Board(int,string,string){
 }
 
 EasyBoard::EasyBoard(int,string,string,string){
-  
-  void set_answer_board = string;
-  string get_unawnsered_board;
+    void set_answer_board = string;
+    string get_unawnsered_board;
 
 }
 EasyBoard::set_answer_board(string){
@@ -42,5 +42,54 @@ EasyBoard::get_unawnsered_board(string){
         "  2  |          |         |\n"
         "     |          |         |\n"
         "     ---------------------\n";
+        string matrixEasyBoard[2][2] = {{A,B},{B,A}};
+        for (int i = 0; i<2, i++)
+            for(int j = 0; j<2; j++){
+                symbol = matrixEasyBoard[i][j];
+                if card _x == 1 && card _y == 1{
+                    "            1         2      \n"
+                    "     ------------------------\n" 
+                    "     |           |           |\n"
+                    "  1  |     A     |           |\n"
+                    "     |           |           |\n"
+                    "     -------------------------\n" 
+                    "     |           |           |\n"
+                    "  2  |           |           |\n"
+                    "     |           |           |\n"
+                    "     ------------------------\n"};
+                if card _x == 1 && card _y == 2{
+                    "            1         2      \n"
+                    "     ------------------------\n" 
+                    "     |           |           |\n"
+                    "  1  |           |    B      |\n"
+                    "     |           |           |\n"
+                    "     -------------------------\n" 
+                    "     |           |           |\n"
+                    "  2  |           |           |\n"
+                    "     |           |           |\n"
+                    "     ------------------------\n"};
+                 if card _x == 2 && card _y == 1{
+                    "            1         2      \n"
+                    "     ------------------------\n" 
+                    "     |           |           |\n"
+                    "  1  |           |           |\n"
+                    "     |           |           |\n"
+                    "     -------------------------\n" 
+                    "     |           |           |\n"
+                    "  2  |     B     |           |\n"
+                    "     |           |           |\n"
+                    "     ------------------------\n"};
+                 if card _x == 2 && card _y == 2;
+                    "            1         2      \n"
+                    "     ------------------------\n" 
+                    "     |           |           |\n"
+                    "  1  |           |           |\n"
+                    "     |           |           |\n"
+                    "     -------------------------\n" 
+                    "     |           |           |\n"
+                    "  2  |           |     B     |\n"
+                    "     |           |           |\n"
+                    "     ------------------------\n"    
+            }
     return unanswered_board;
 }
