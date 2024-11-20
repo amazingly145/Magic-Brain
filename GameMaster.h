4645a07e90bfa9a2a1c35d091a7ac0b25cc2ee16
amazingly145 <A01753419@tec.mx>
2024-11-20 00:42:24 -0600

    Update GameMaster.h

--- GameMaster.h
+++ GameMaster.h
@@ -1,6 +1,8 @@
 #include <string>
 using namespace std;
 #include <iostream>
+#include "EasyBoard.cpp"
+#include "HardBoard.cpp"
 #ifndef GAMEMASTER_H
 #define GAMEMASTER_H
 class GameMaster(){
@@ -8,11 +10,12 @@ class GameMaster(){
        hardboard(HardBoard);
        easyboard(EasyBoard);
     private:
-        string Card(int,int);
-        bool GameMaster();
+        string Card(int,int,array);
+        GameMaster(bool);
+        bool equality_pair(string);
         void setShuffle_card(random);
         void setStart_game(bool);
-        int getScore();
+        int getincrement_score();
         bool getequailty_card();
         int getchoose_level();
 };
