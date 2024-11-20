bd0d09dc7ba7c639b1095c9f4549b950a548bf4c
amazingly145 <A01753419@tec.mx>
2024-11-20 00:39:06 -0600

    Update Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -8,12 +8,12 @@ Card::Card(string,int,int){
     int side_y = _side_y;
 }
 Card::int set_side_x(int){
-    int side_x = void;
+    int _side_x = void;
 }
 Card::int set_side_y(int){
-    int side_y = void;
+    int _side_y = void;
 
 }
 Card::string set_symbol(){
-    int_set_symbol = void
+    array _set_symbol = void
 }
