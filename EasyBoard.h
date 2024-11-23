dbbe84e2dda325814f10482d7ef367b63cccc986
amazingly145 <A01753419@tec.mx>
2024-11-23 09:39:27 -0600

    Update EasyBoard.h

--- EasyBoard.h
+++ EasyBoard.h
@@ -7,15 +7,21 @@ using namespace std;
 #define EASYBOARD_H
 class EasyBoard : public Board{
     //Atributos protegidos que hereda de la clase padre (Board)
+    //Hereda todos los atributos de la clase board
     protected:
+        //Atribute of the class EasyBoard
         string equality_pair;
     public:
+        //Constructor of the inherited attributes of superclass Board.
+        EasyBoard(int, string, int, string, int, string);
+        //Constructor of easyboard
         EasyBoard();
-        EasyBoard(int, string, int, string, string, int, string, int, int);
-        getSide_x();
+        //Setters
+        void setDatalevel(int, string, int, string, int, string);
         //Getter
         string getEquality_pair();
         int getPlayer_card_1();
         int getPlayer_card_2();
+        string getDatalevel();
 };
 #endif
