191a07f9c280f13bbce065dadf03b0272a2b46bc
amazingly145 <A01753419@tec.mx>
2024-11-22 20:29:43 -0600

    Update EasyBoard.h

--- EasyBoard.h
+++ EasyBoard.h
@@ -1,13 +1,21 @@
 #include <string>
 using namespace std;
 #include <iostream>
+#include "Board.h"
+#include "Card.cpp"
 #ifndef EASYBOARD_H
 #define EASYBOARD_H
-class EasyBoard(){
+class EasyBoard : public Board{
+    //Atributos protegidos que hereda de la clase padre (Board)
+    protected:
+        string equality_pair;
     public:
-        EasyBoard(int,string, string, string);
-        void setis_a_winner(int);
-        void setanwer_board(string);
-        string getunawnsered_board();
+        EasyBoard();
+        EasyBoard(int, string, int, string, string, int, string, int, int);
+        getSide_x();
+        //Getter
+        string getEquality_pair();
+        int getPlayer_card_1();
+        int getPlayer_card_2();
 };
 #endif
