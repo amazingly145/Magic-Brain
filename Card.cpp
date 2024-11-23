fb65e5fe4fc6a6b80e5768fe6bcba37e6fb1fc0e
amazingly145 <A01753419@tec.mx>
2024-11-23 09:37:40 -0600

    Update Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -2,7 +2,9 @@
 #include <iostream>
 using namespace std;
 #include <string>
+
 Card::Card(){
+    symbol = "";
     side_x = 0;
     side_y = 0;
 }
@@ -11,10 +13,15 @@ Card::Card(string _symbol,int _side_x, int _side_y){
     side_x = _side_x;
     side_y = _side_y;
 }
-//Revisar si quitar
 //Métodos setters
 void Card::setSymbol(string _symbol){
     symbol=_symbol;
+}
+void Card::setSide_x(int _side_x){
+    side_x = _side_x;
+}
+void Card::setSide_y(int _side_y){
+    side_y = _side_y;
 }
 //Métodos getters
 int Card::getSide_x(){
@@ -22,4 +29,9 @@ int Card::getSide_x(){
 }
 int Card::getSide_y(){
     return side_y;
+}
+string Card::getCardData(){
+    string CardData = "Card in side x: " + to_string(side_x) +
+    "Card in side y:" + to_string(side_y);
+    return CardData;
 }
