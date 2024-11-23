67064c8217da5a15d14a5b11b3451c6992b44263
amazingly145 <A01753419@tec.mx>
2024-11-23 09:43:35 -0600

    Update GameMaster.cpp

--- GameMaster.cpp
+++ GameMaster.cpp
@@ -1,258 +1,79 @@
 #include "GameMaster.h"
-#include "EasyBoard.h"
-#include "HardBoard.h"
-#include "Card.h"
-#include "Board.h"
+#include "Easyboard.h"
 #include <iostream>
 using namespace std;
 #include <string>
-GameMaster::GameMaster(EasyBoard, HardBoard){
-    HardBoard(hardboard);
-    EasyBoard(easyboard);
-    int score = _score;
-    string player_move;
-    bool equality_pair(string);
-    int player_card_1;
-    int player_card_2;
+GameMaster::GameMaster(){
+    player_decision = 0;
 }
-GameMaster::GameMaster(bool){
-    int GameMaster;
-    string player_move = _player_move;
-    bool equality_pair = _equaliy_pair;
-    _score = 0;
-    player_card_1 = _player_card_1;
-    player_card_2 = _player_card_2;
+GameMaster::GameMaster(int _player_decision){
+    player_decision = _player_decision;
 }
-GameMaster::setshuffle_cards(random){
-    setshuffle_cards = random;
-}
-GameMaster::_equailty_pair(string):EasyBoard(string unanswered_board){
-    if int(_player_card_1 == 1 && _player_card_2 == 2){
-        "            1         2      \n"
-        "     ------------------------\n" 
-        "     |           |           |\n"
-        "  1  |     A     |           |\n"
-        "     |           |           |\n"
-        "     -------------------------\n" 
-        "     |           |           |\n"
-        "  2  |           |     A     |\n"
-        "     |           |           |\n"
-        "     ------------------------\n"
-        _score +=1
+//Métodos setters
+void GameMaster::setPlayer_decision(int _player_decision){
+    //Si el usuario escoge 1, yo quiero asignarle el valor que me de.
+    if (_player_decision == 1){
+        easyboard.setNum_levelboard(1);
     }
-if int(_player_card_1 == 2 && _player_card_2 == 1){
-        "            1         2      \n"
-        "     ------------------------\n" 
-        "     |           |           |\n"
-        "  1  |           |    B      |\n"
-        "     |           |           |\n"
-        "     -------------------------\n" 
-        "     |           |           |\n"
-        "  2  |     B     |           |\n"
-        "     |           |           |\n"
-        "     ------------------------\n"
-        _score +=1
-    };
-}
-GameMaster::_equailty_pair(string):HardBoard(string unanswered_board){
-    if int(_player_card_1 == 1 && _player_card_2 == 3){
-        "        1             2           3     \n"
-        "    ------------------------------------\n"
-        "    |           |           |          |\n"
-        "  1 |    A      |           |          |\n"
-        "    |           |           |          |\n"
-        "    ------------------------------------\n"
-        "    |           |           |          |\n"
-        "  2 |           |           |    A     |\n"
-        "    |           |           |          |\n"
-        "    ------------------------------------\n";
-        _score +=1
-    };
-    if int(_player_card_1 == 2 && _player_card_2 == 1){
-        "        1             2           3     \n"
-        "    ------------------------------------\n"
-        "    |           |           |          |\n"
-        "  1 |           |    B      |          |\n"
-        "    |           |           |          |\n"
-        "    ------------------------------------\n"
-        "    |           |           |          |\n"
-        "  2 |    B      |           |          |\n"
-        "    |           |           |          |\n"
-        "    ------------------------------------\n";
-        _score +=1
-    };
-    if int(_player_card_1 == 3 && _player_card_2 == 2){
-        "        1             2           3     \n"
-        "    ------------------------------------\n"
-        "    |           |           |          |\n"
-        "  1 |           |           |    C     |\n"
-        "    |           |           |          |\n"
-        "    ------------------------------------\n"
-        "    |           |           |          |\n"
-        "  2 |           |     C     |          |\n"
-        "    |           |           |          |\n"
-        "    ------------------------------------\n";
-        _score +=1
+    if (_player_decision == 2){
+        hardboard.setNum_levelboard(2);
     }
 }
-GameMaster::void setstart_game(bool){
-    if (user=="start"){
-        void start_game = true;
+int GameMaster::getPlayer_decision(){
+    //Si el jugador escoge 1 o 2, quiero que imprime sus respectivos tableros.
+    if (player_decision == 1){
+        return easyboard.getUnanswer_board();
+    }
+    if (player_decision == 2){
+        return hardboard.getUnanswer_board();
     }
 }
-GameMaster::int getincrementscore(){
-    int max_score = 6;
-    if EasyBoard(score = 2){
-        score = True;
-    }
-    if HardBoard(score = 3){
-        score = True;
-    }
-}
-GameMaster::bool getequality_card(){
-    if (answerboard == unanswered_board){
-        return true;
-    }
-}
-GameMaster::int getchoose_level(){
-    if (int user == 1){
-        EasyBoard();
-    }
-    if (int user == 2){
-        HardBoard();
+
+void GameMaster::setPlaymode_easyboard(int score){
+    score = 0;
+    while (score>=0){
+        easyboard.getLevelData();
+        easyboard.getUnanswer_board();
+        cout<<"Choose Card 1"<<endl;
+        cout<<"Choose x axis"<<endl;
+        int side_x;
+        cin>>side_x;
+        cout<<"Choose y axis"<<endl;
+        int side_y;
+        cin>>side_y;
+        easyboard.getplayer_card_1(side_x,side_y)
+        easyboard.getplayer_card_2(side_x,side_y)
+        cout<<"Write your card in the y axis: "<<endl;
     }
 }
-GameMaster::GameMaster(string player_move):Card(int _x, int_y):EasyBoard(){
-    if Card(_x) == 1 && Card(_y) == 1{
-        string player_move_1= 
-            "            1         2      \n"
-            "     ------------------------\n" 
-            "     |           |           |\n"
-            "  1  |     A     |           |\n"
-            "     |           |           |\n"
-            "     -------------------------\n" 
-            "     |           |           |\n"
-            "  2  |           |           |\n"
-            "     |           |           |\n"
-            "     ------------------------\n";
-        player_card_1=1};
-    if Card(_x) == 1 && Card(_y) == 2{
-        string player_move_1= 
-            "            1         2      \n"
-            "     ------------------------\n" 
-            "     |           |           |\n"
-            "  1  |           |     B     |\n"
-            "     |           |           |\n"
-            "     -------------------------\n" 
-            "     |           |           |\n"
-            "  2  |           |           |\n"
-            "     |           |           |\n"
-            "     ------------------------\n";
-        player_card_1=2};
-    if Card(_x) == 2 && Card(_y) == 1{
-        string player_move= 
-            "            1         2      \n"
-            "     ------------------------\n" 
-            "     |           |           |\n"
-            "  1  |           |           |\n"
-            "     |           |           |\n"
-            "     -------------------------\n" 
-            "     |           |           |\n"
-            "  2  |     B     |           |\n"
-            "     |           |           |\n"
-            "     ------------------------\n";
-        player_card_2=1};
-    if Card(_x) == 2 && Card(_y) == 2{
-        string player_move = 
-            "            1         2      \n"
-            "     ------------------------\n" 
-            "     |           |           |\n"
-            "  1  |           |           |\n"
-            "     |           |           |\n"
-            "     -------------------------\n" 
-            "     |           |           |\n"
-            "  2  |           |     A     |\n"
-            "     |           |           |\n"
-            "     ------------------------\n";
-        player_card_2=2};
+
+void GameMaster::setPlaymodeEasyboard(int score){
+    score = 0;
+    while (score>=0){
+        hardboard.getLevelData();
+        hardboard.getUnanswer_board();
+        cout<<"Choose Card 1"<<endl;
+        cout<<"Choose x axis"<<endl;
+        int side_x;
+        cin>>side_x;
+        cout<<"Choose y axis"<<endl;
+        int side_y;
+        cin>>side_y;
+        easyboard.getplayer_card_1(side_x,side_y);
+        easyboard.getplayer_card_2(side_x,side_y);
+        cout<<"Write your card in the y axis: "<<endl;
+        score += 1;
+    }
 }
-GameMaster::GameMaster(string player_move):Card(int _x, int_y):HardBoard(){
-    if Card(_x) == 1 && Card(_y) == 1{
-        string player_move_1 = 
-            "        1             2           3     \n"
-            "    ------------------------------------\n"
-            "    |           |           |          |\n"
-            "  1 |    A      |           |          |\n"
-            "    |           |           |          |\n"
-            "    ------------------------------------\n"
-            "    |           |           |          |\n"
-            "  2 |           |           |          |\n"
-            "    |           |           |          |\n"
-            "    ------------------------------------\n";
-        player_card_1=1};
-    if Card(_x) == 1 && Card(_y) == 2{
-        string player_move_1 = 
-            "        1             2           3     \n"
-            "    ------------------------------------\n"
-            "    |           |           |          |\n"
-            "  1 |           |     B     |          |\n"
-            "    |           |           |          |\n"
-            "    ------------------------------------\n"
-            "    |           |           |          |\n"
-            "  2 |           |           |          |\n"
-            "    |           |           |          |\n"
-            "    ------------------------------------\n";
-        player_card_1=2};
-    if Card(_x) == 1 && Card(_y) == 3{
-        string player_move_1 = 
-            "        1             2           3     \n"
-            "    ------------------------------------\n"
-            "    |           |           |          |\n"
-            "  1 |           |           |     C    |\n"
-            "    |           |           |          |\n"
-            "    ------------------------------------\n"
-            "    |           |           |          |\n"
-            "  2 |           |           |          |\n"
-            "    |           |           |          |\n"
-            "    ------------------------------------\n";
-        player_card_1=3};
-    if Card(_x) == 1 && Card(_y) == 1{
-        string player_move_1 = 
-            "        1             2           3     \n"
-            "    ------------------------------------\n"
-            "    |           |           |          |\n"
-            "  1 |           |           |          |\n"
-            "    |           |           |          |\n"
-            "    ------------------------------------\n"
-            "    |           |           |          |\n"
-            "  2 |     B     |           |          |\n"
-            "    |           |           |          |\n"
-            "    ------------------------------------\n";
-        player_card_2=1};
-    if Card(_x) == 1 && Card(_y) == 1{
-        string player_move_1 = 
-            "        1             2           3     \n"
-            "    ------------------------------------\n"
-            "    |           |           |          |\n"
-            "  1 |           |           |          |\n"
-            "    |           |           |          |\n"
-            "    ------------------------------------\n"
-            "    |           |           |          |\n"
-            "  2 |           |     C     |          |\n"
-            "    |           |           |          |\n"
-            "    ------------------------------------\n";
-        player_card_2=2};
-    if Card(_x) == 1 && Card(_y) == 1{
-        string player_move_1 = 
-            "        1             2           3     \n"
-            "    ------------------------------------\n"
-            "    |           |           |          |\n"
-            "  1 |           |           |          |\n"
-            "    |           |           |          |\n"
-            "    ------------------------------------\n"
-            "    |           |           |          |\n"
-            "  2 |           |           |     A    |\n"
-            "    |           |           |          |\n"
-            "    ------------------------------------\n";
-        player_card_2=3};
+int GameMaster::getPlay(){
+    int score = 3
+    while (score >= 3){
+        if (_player_decision == 1){
+            easyboard.getUnanswer_board();
+            cout<<"Choose Card 1"<<endl;
+            cout<<"Write your card in the x axis"<<endl;
+
+        }
+        return 
+    }
 }
