b4b918bde269ceea1da61672761b38593b1afea9
amazingly145 <A01753419@tec.mx>
2024-11-23 09:40:41 -0600

    Update HardBoard.h

--- HardBoard.h
+++ HardBoard.h
@@ -1,13 +1,21 @@
 #include <string>
+#include "Board.h"
+#include "Card.h"
 using namespace std;
 #include <iostream>
 #ifndef HARDBOARD_H
 #define HARDBOARD_H
-class HardBoard(){
+class HardBoard : public Board{
+    //Atributos que van a heredar
+    protected:
+        string equality_pair;
     public:
-        HardBoard(int,string, string, string);
-        void setis_a_winner(int);
-        void setanwer_board(string);
-        string getunawnsered_board();
+        HardBoard(int, string, int, string, int, string);
+        HardBoard();
+        void setDatalevel(int, string, int, string, int, string);
+        string getEquality_pair();
+        int getPlayer_card_1();
+        int getPlayer_card_2();
+        string getDatalevel();
 };
 #endif
