69b05bb07994de3903f5d46e4f4d3d82e92b42f7
amazingly145 <A01753419@tec.mx>
2024-11-23 17:00:10 -0600

    Update GameMaster.h

--- GameMaster.h
+++ GameMaster.h
@@ -1,28 +1,21 @@
-#include <string>
-using namespace std;
-#include <iostream>
-#include "EasyBoard.cpp"
-#include "HardBoard.cpp"
 #ifndef GAMEMASTER_H
 #define GAMEMASTER_H
-class GameMaster{
-    //Atributos
-    private:
-        //Atributo de la propia clase.
-        int player_decision;
-        //Atributo de tipo composición de las subclases EasyBoard y HardBoard.
-        HardBoard playmode_hardboard;
-        EasyBoard playmode_easyboard;
-    //Métodos
-    public:
-        //Constructores 
-        GameMaster();
-        GameMaster(int);
-        //Métodos de tipo constructor
-        int playmode_easyboard(int);
-        int playmode_hardboard(int);
-        void setPlayer_decision(int);
-        int getPlayer_decision();
-        void setPlay(int,int);    
+
+#include "EasyBoard.h"
+#include "HardBoard.h"
+
+class GameMaster {
+private:
+    EasyBoard easyBoard;
+    HardBoard hardBoard;
+
+public:
+    GameMaster();
+    void setPlaymodeEasyboard(int &score);
+    void setPlaymodeHardboard(int &score);
+    string processMove(int level, int x1, int y1, int x2, int y2, int &score);
+    void displayBoard(int level);
+    bool isGameComplete(int level); // Checks if all cards are revealed
 };
+
 #endif
