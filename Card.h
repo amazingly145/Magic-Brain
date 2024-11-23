c6259058a065d1c224989e5eb6be097f5fb64db6
amazingly145 <A01753419@tec.mx>
2024-11-23 09:37:00 -0600

    Update Card.h

--- Card.h
+++ Card.h
@@ -1,8 +1,13 @@
+//Library names
+#ifndef CARD_H
+#define CARD_H
+#include "Card.cpp"
+
 #include <string>
 using namespace std;
 #include <iostream>
-#ifndef CARD_H
-#define CARD_H
+
+//Class definition
 class Card{
     //Atributos.
     private:
@@ -11,10 +16,16 @@ class Card{
         int side_y;
     //Métodos.
     public:
+        //Constructores
         Card();
         Card(string symbol, int side_x,int side_y);
+        //Getteres
         int getSide_x( );
         int getSide_y( );
+        string getCardData();
+        //Setters
+        void setSide_x(int);
+        void setSide_y(int);
         void setSymbol(string);
 };
 #endif
