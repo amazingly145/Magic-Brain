f360dddcae7d27b40b56d99a3764ff5ee8bdc2f3
amazingly145 <A01753419@tec.mx>
2024-11-23 17:03:32 -0600

    Update Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -1,37 +1,48 @@
 #include "Card.h"
-#include <iostream>
-using namespace std;
-#include <string>
 
-Card::Card(){
-    symbol = "";
-    side_x = 0;
-    side_y = 0;
-}
-Card::Card(string _symbol,int _side_x, int _side_y){
+// Constructor por defecto con los valores a inicializar
+Card::Card() : symbol(""), side_x(0), side_y(0) {}
+
+// Configurar los atributos para que se asignen los valores iniciales
+Card::Card(string _symbol, int _side_x, int _side_y) 
+    : symbol(_symbol), side_x(_side_x), side_y(_side_y) {}
+
+
+// Métodos Setters
+
+// Configurar el símbolo de la carta
+void Card::setSymbol(string _symbol) {
     symbol = _symbol;
-    side_x = _side_x;
-    side_y = _side_y;
 }
-//Métodos setters
-void Card::setSymbol(string _symbol){
-    symbol=_symbol;
-}
-void Card::setSide_x(int _side_x){
+
+// Configurar la posición en x de la carta
+void Card::setSide_x(int _side_x) {
     side_x = _side_x;
 }
-void Card::setSide_y(int _side_y){
+
+// Configurar la posición en y de la carta
+void Card::setSide_y(int _side_y) {
     side_y = _side_y;
 }
-//Métodos getters
-int Card::getSide_x(){
+
+// Métodos Getters
+
+// Obtener la posición en x de la carta
+int Card::getSide_x() {
     return side_x;
 }
-int Card::getSide_y(){
+
+// Obtener la posición en y de la carta
+int Card::getSide_y() {
     return side_y;
 }
-string Card::getCardData(){
-    string CardData = "Card in side x: " + to_string(side_x) +
-    "Card in side y:" + to_string(side_y);
-    return CardData;
+
+// Obtener el símbolo de la carta
+string Card::getSymbol() {
+    return symbol;
+}
+
+// Obtener los datos de la carta
+string Card::getCardData() {
+    return "Card Symbol: " + symbol + " at (" + to_string(side_x) + ", " + to_string(side_y) + ")";
 }
