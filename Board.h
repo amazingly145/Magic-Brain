e7186291367686591ab4f6106175e46c0627ae4a
amazingly145 <A01753419@tec.mx>
2024-11-23 17:04:26 -0600

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,44 +1,36 @@
-//library names
 #ifndef BOARD_H
 #define BOARD_H
 
+#include "Card.h"
 #include <string>
 using namespace std;
-#include <iostream>
-#include "Card.cpp"
 
-//Class definition
-class Board{
-    //Atributes
-    protected:
-        //Setteres
-        int num_levelboard;
-        string answer_board;
-        int num_cards;
-        //Getters
-        string name;
-        int score;
-        string unanswer_board;
-        //Composition of cards
-        Card cards_x;
-        Card cards_y;
-    //Methods 
-    public:
-        //Constructors
-        Board();
-        Board(int, string, int, string, int, string);//Constructor de la clase Board o los atributos de la clase board
-        //Composition of cards
-        string Card(int,int);
-        //Setters
-        void setNum_levelboard(int);
-        void setAnswer_board(string);
-        void setNum_cards(int);
-        void setMatriz_board(string);
-        //Getters
-        string getName(); 
-        int getScore();  
-        string getUnanswer_board();
-        int getSide_x();
-        int getSide_y();   
+// Clase Board
+class Board {
+// Atributos
+protected:
+    // Atributos de la clase
+    int num_levelboard;
+    string answer_board;
+    int num_cards;
+    string name;
+    int score;
+    string unanswer_board;
+// Métodos
+public:
+    // Constructores
+    Board();
+    Board(int num_levelboard, string answer_board, int num_cards, string name, int score, string unanswer_board);
+
+    // Métodos Setters
+    void setNum_levelboard(int num_levelboard);
+    void setAnswer_board(string answer_board);
+    void setNum_cards(int num_cards);
+    
+    // Métodos Getters
+    string getName();
+    int getScore();
+    string getUnanswer_board();
 };
+
 #endif
