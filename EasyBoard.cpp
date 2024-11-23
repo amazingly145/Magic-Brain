f3863b04c82383b26004e66a7ed321abcda175be
amazingly145 <A01753419@tec.mx>
2024-11-23 09:40:13 -0600

    Update EasyBoard.cpp

--- EasyBoard.cpp
+++ EasyBoard.cpp
@@ -2,153 +2,77 @@
 #include <string>
 using namespace std;
 #include <iostream>
+//Inicializar los valores, con respecto a la super clase, board.
+//En el constructor van los atributos con los qu va inicializar.
+//Los parámetros que va a recibir en el main.
+//Los nuevos atributos de la subclase Easyboard con referencia a board.
 //default constructor to begin inheritance
 //Begin the values of the number level, cards and name.
 //Los atributos que se heredan de los hijos no tienes que especificar el tipo de variable.
 //Se pone el tipo de retorno de los metódos del archivo .h
-EasyBoard::EasyBoard():Board(){
-    score = 0;
+EasyBoard::EasyBoard(){
     equality_pair= "";
-    EasyBoard.getSide_x(player_move_x);
-
 }
-//Inicializar los valores, con respecto a la super clase, board.
-//En el constructor van los atributos con los qu va inicializar.
-//Los parámetros que va a recibir en el main.
-EasyBoard::EasyBoard(int num_levelboard_, string answer_board_, int num_cards_, string matriz_board_, string name_, int score_, string unanswer_board_, int side_x_, int side_y_): Board(num_levelboard_, answer_board_, num_cards_, matriz_board_, name_, score_, unanswer_board_, side_x_, side_y_){
+//Constructor con los valores de la clase padre de Board
+EasyBoard::EasyBoard(int num_levelboard_, string answer_board_, int num_cards_, string name_, int score_, string unanswer_board_): Board(num_levelboard_, answer_board_, num_cards_,name_, score_, unanswer_board_){
     equality_pair = _equality_pair;
-    
 }
-int EasyBoard::getNum_level_board_(){
-    return num_levelboard_= 1;
+//Setters
+//Los valores asignados a el nivel, correspondientes.
+void EasyBoard::setDatalevel(int num_levelboard_, string answer_board_, int num_cards_, string name_, int score_, string unanswer_board_){
+    num_levelboard_= 1;
+    answer_board_ = "A B \nB A";
+    num_cards_= 4;
+    name_ = "EasyLevel";
+    score_ = 0;
+    unanswer_board_ = "* *\n* *";
 }
-string EasyBoard::getAnswer_board_(){
-    return answer_board_ = 
-        "          1          2    \n"
-        "     ---------------------\n"
-        "     |          |         |\n"    
-        "  1  |    A     |    B    |\n"
-        "     |          |         |\n"
-        "     ---------------------\n"
-        "     |          |         |\n"    
-        "  2  |     B    |    A    |\n"
-        "     |          |         |\n"
-        "     ---------------------\n";
-}
-int EasyBoard::getNum_cards_(){
-    return num_cards_= 4;
-}
-string EasyBoard::getMatriz_board_(){
-    return matriz_board_[2][2] = {{A,B},{B,A}};
-}
-string EasyBoard::getName_(){
-    return name_ = "EasyLevel";
-}
-string EasyBoard::getUnanswer_board_(){
-    return unanswer_board_ = 
-    "          1          2    \n"
-    "     ---------------------\n"
-    "     |          |         |\n"    
-    "  1  |          |         |\n"
-    "     |          |         |\n"
-    "     ---------------------\n"
-    "     |          |         |\n"    
-    "  2  |          |         |\n"
-    "     |          |         |\n"
-    "     ---------------------\n";
-}
+//Getters
 //Constructor of the moves of the player.
-//Constructor getter de player_card_1.
-int EasyBoard::getPlayer_card_1(){
-    if (player_move_x == 1 && EasyBoard.getSide_y()== 1){
-        string player_move_1= 
-            "            1         2      \n"
-            "     ------------------------\n" 
-            "     |           |           |\n"
-            "  1  |     A     |           |\n"
-            "     |           |           |\n"
-            "     -------------------------\n" 
-            "     |           |           |\n"
-            "  2  |           |           |\n"
-            "     |           |           |\n"
-            "     ------------------------\n";
-        player_card_1=1};
-    if (EasyBoard.getside_x() == 1 && EasyBoard.getSide_y() == 2){
-        string player_move_1= 
-            "            1         2      \n"
-            "     ------------------------\n" 
-            "     |           |           |\n"
-            "  1  |           |     B     |\n"
-            "     |           |           |\n"
-            "     -------------------------\n" 
-            "     |           |           |\n"
-            "  2  |           |           |\n"
-            "     |           |           |\n"
-            "     ------------------------\n";
-        player_card_1=2};
-    return player_card_1;
-}
-int EasyBoard::getPlayer_card_2(){
-    if (EasyBoard.getSide_x() == 2 && EasyBoard.getsSide_y() == 1){
-        string player_move= 
-            "            1         2      \n"
-            "     ------------------------\n" 
-            "     |           |           |\n"
-            "  1  |           |           |\n"
-            "     |           |           |\n"
-            "     -------------------------\n" 
-            "     |           |           |\n"
-            "  2  |     B     |           |\n"
-            "     |           |           |\n"
-            "     ------------------------\n";
-        player_card_2=1};
-    if (EasyBoard.getSide_x() == 2 && EasyBoard.getSide_y() == 2){
-        string player_move = 
-            "            1         2      \n"
-            "     ------------------------\n" 
-            "     |           |           |\n"
-            "  1  |           |           |\n"
-            "     |           |           |\n"
-            "     -------------------------\n" 
-            "     |           |           |\n"
-            "  2  |           |     A     |\n"
-            "     |           |           |\n"
-            "     ------------------------\n";
-        player_card_2=2};
-    return player_card_2;
-}
-string EasyBoard::getEquailty_pair(){
-    //Mover todas las cosas de la clase de easyboard y hardboard
-    //Agregarle métodos para cada tablero.
-    if (int player_card_1 == 1 && int player_card_2 == 2){
-        pair = 
-            "            1         2      \n"
-            "     ------------------------\n" 
-            "     |           |           |\n"
-            "  1  |     A     |           |\n"
-            "     |           |           |\n"
-            "     -------------------------\n" 
-            "     |           |           |\n"
-            "  2  |           |     A     |\n"
-            "     |           |           |\n"
-            "     ------------------------\n"
-            EasyBoard.getScore_ +=1
+int EasyBoard::player_card_1(int side_x, int side_y) {
+    if (side_x == 1 && side_y == 1) {
+        string player_move_1 = "A *\n* *";
+        int player_card_1 == 1;
+        return player_card_1;
     }
-    if (int player_card_1 == 2 && int player_card_2 == 1){
-        "            1         2      \n"
-        "     ------------------------\n" 
-        "     |           |           |\n"
-        "  1  |           |    B      |\n"
-        "     |           |           |\n"
-        "     -------------------------\n" 
-        "     |           |           |\n"
-        "  2  |     B     |           |\n"
-        "     |           |           |\n"
-        "     ------------------------\n"
-        getScore +=1
-    };
+    if (side_x == 1 && side_y == 2) {
+        string player_move_1 = "* B\n* *";
+        int player_card_1 == 2;
+        return player_card_1;
+    }
+    return -1; // Valor por defecto en caso de error
 }
-
+int EasyBoard::player_card_2(int cards_x_, int cards_y_) {
+    if (cards_x_.getSide_x_() == 2 && cards_y_.getSide_y_() == 1) {
+        string player_move_2 = "* *\nB *"
+        int player_card_2 == 1;
+        return player_card_2;
+    }
+    if (cards_x_.getSide_x_() == 2 && cards_y_.getSide_y_() == 2) {
+        string player_move_2 = "* *\n* A";
+        int player_card_2 == 2;
+        return player_card_2;
+    }
+    return -1; // Valor por defecto en caso de error
+}
+//Constructor getter de player_card_1.
+string EasyBoard::getEquality_pair(int player_card_1, int player_card_2) {
+    if (player_card_1 == 1 && player_card_2 == 2) {
+        string pair = "A *\n* A";
+        score_ += 1;
+        return pair;
+    }else{
+        return "No match!";
+    }
+    if (player_card_1 == 2 && player_card_2 == 1) {
+        string pair = "* B\nB *";
+        score_ += 1;
+        return pair;
+    }else{
+        return "No match!";
+}
 string EasyBoard::getLevelData(){
-    //getter con los datos
+    string EasyBoardData = "Welcome to: " + name_ + "This is level: " + to_string(num_levelboard_) + "There are: " + to_string(num_cards_);
+    return EasyBoardData;
+}
 }
