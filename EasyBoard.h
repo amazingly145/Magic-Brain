b39e9fa268005077b5b1949efac371fdc171c132
amazingly145 <A01753419@tec.mx>
2024-11-23 17:01:11 -0600

    Update EasyBoard.h

--- EasyBoard.h
+++ EasyBoard.h
@@ -1,27 +1,18 @@
-#include <string>
-using namespace std;
-#include <iostream>
-#include "Board.h"
-#include "Card.cpp"
 #ifndef EASYBOARD_H
 #define EASYBOARD_H
-class EasyBoard : public Board{
-    //Atributos protegidos que hereda de la clase padre (Board)
-    //Hereda todos los atributos de la clase board
-    protected:
-        //Atribute of the class EasyBoard
-        string equality_pair;
-    public:
-        //Constructor of the inherited attributes of superclass Board.
-        EasyBoard(int, string, int, string, int, string);
-        //Constructor of easyboard
-        EasyBoard();
-        //Setters
-        void setDatalevel(int, string, int, string, int, string);
-        //Getter
-        string getEquality_pair();
-        int getPlayer_card_1();
-        int getPlayer_card_2();
-        string getDatalevel();
+
+#include "Board.h"
+#include <string>
+using namespace std;
+
+// Clase EasyBoard
+class EasyBoard : public Board {
+public:
+    EasyBoard();
+    EasyBoard(int num_levelboard, string answer_board, int num_cards, string name, int score, string unanswer_board);
+
+    string revealCard(int x, int y); // Reveals the card at given coordinates
+    void updateBoard(int x, int y, string card); // Updates the unanswer_board with the revealed card
 };
+
 #endif
